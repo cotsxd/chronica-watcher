1b27eab99423cd548cdc267dd1bc8883fa624fc9
cotsxd <79633317+cotsxd@users.noreply.github.com>
2026-05-21 02:22:32 +0100

    Delete UPLOAD TO GITHUB.cmd

--- UPLOAD TO GITHUB.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-echo Chronica Discord Watcher GitHub uploader
-echo.
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\upload-to-github.ps1"
-echo.
-pause
